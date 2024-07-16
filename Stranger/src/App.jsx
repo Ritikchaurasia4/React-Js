856519b1a29e52e4325a07d9d1dc17cec4c2b04c
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-07-16 20:02:42 +0530

    Making Netflix cards in react

---
+++ Stranger/src/App.jsx
@@ -0,0 +1,26 @@
+export const App = ()=>{
+  return (
+    
+<div>
+   <NetflixSeries/>
+
+   <NetflixSeries/>
+
+   <NetflixSeries/>
+</div>
+ );
+}; 
+
+export const NetflixSeries =()=>{
+  return (
+    <div> 
+   <div>
+     <img src="Stranger.jpg" alt="stranger"  width="40%" height="20%"/>
+   </div>
+     <h2>Name: Strangers Things</h2>
+     <h3>Rating: 8.7</h3>
+     <p> Summary : This is a funtastic series. This is a fantasy series. </p>
+  </div>
+  )
+
+}

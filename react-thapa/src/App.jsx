eb53c03ec7a728ef5ae1914e534b611adf3062e7
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-07-16 17:30:36 +0530

    another way to write code in react

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,3 +1,11 @@
+// export const App = ()=>{
+//   return <h1> This is a react page</h1>;
+// };
+
+
+// <h1> This is a react Page </h1>  : In pure react we can also write it(h1) as React.createElement('h1',null, This is a react page)
+
+import React from "react";
 export const App = ()=>{
-  return <h1> This is a react page</h1>;
+  return React.createElement('h1',null,"This is the react page");
 };

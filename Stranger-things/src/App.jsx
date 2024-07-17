0b378eb044e6a544c4b41173c71f3b2eb2a6d270
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-07-17 17:01:46 +0530

    Using Fragments

---
+++ Stranger-things/src/App.jsx
@@ -0,0 +1,132 @@
+
+// export const App = ()=>{
+//   return(
+//     <div>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//      </div>
+//   );
+  
+// };
+
+// export const NetfelixSeries=()=>{
+//   return(
+//   <div>
+//     <img src="Stranger.jpg" alt="Stranger" width="30%" height="30%"/>
+//     <h3>Strangers Things</h3>
+//     <h3>Rating:8.5</h3>
+//     <p>This is one of the best Series on Netfelix</p>
+//   </div>)
+// }
+
+// ---------------------------------------------------------------
+
+// we can also write the above code as given below :
+
+// ---------------------------------------------------------------
+
+// export const App = ()=>{
+//   return([<NetfelixSeries key="1"/> , <NetfelixSeries key="2"/>]
+//   );
+  
+// };
+
+// export const NetfelixSeries=()=>{
+//   return(
+//   <div>
+//     <img src="Stranger.jpg" alt="Stranger" width="30%" height="30%"/>
+//     <h3>Strangers Things</h3>
+//     <h3>Rating:8.5</h3>
+//     <p>This is one of the best Series on Netfelix</p>
+//   </div>)
+// }
+
+
+// ---------------------------------------------------------------
+
+// we can also write the above code as given below : Using Fragment
+
+// ---------------------------------------------------------------
+
+// import React from "react";
+// export const App = ()=>{
+//   return(
+//     <React.Fragment>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//     </React.Fragment>
+//   );
+  
+// };
+
+//  const NetfelixSeries=()=>{
+//   return(
+//   <div>
+//     <img src="Stranger.jpg" alt="Stranger" width="30%" height="30%"/>
+//     <h3>Strangers Things</h3>
+//     <h3>Rating:8.5</h3>
+//     <p>This is one of the best Series on Netfelix</p>
+//   </div>)
+// }
+
+
+// ---------------------------------------------------------------
+
+// we can also write the above code as given below : Using Fragment
+
+// ---------------------------------------------------------------
+
+
+// import {Fragment} from "react";
+// export const App = ()=>{
+//   return(
+//     <Fragment>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//      <NetfelixSeries/>
+//     </Fragment>
+//   );
+  
+// };
+
+//  const NetfelixSeries=()=>{
+//   return(
+//   <div>
+//     <img src="Stranger.jpg" alt="Stranger" width="30%" height="30%"/>
+//     <h3>Strangers Things</h3>
+//     <h3>Rating:8.5</h3>
+//     <p>This is one of the best Series on Netfelix</p>
+//   </div>)
+// }
+
+
+// ---------------------------------------------------------------
+
+// we can also write the above code as given below :
+
+// ---------------------------------------------------------------
+
+
+// import {Fragment} from "react";
+export const App = ()=>{
+  return(
+    <>
+     <NetfelixSeries/>
+     <NetfelixSeries/>
+     <NetfelixSeries/>
+    </>
+  );
+  
+};
+
+ const NetfelixSeries=()=>{
+  return(
+  <div>
+    <img src="Stranger.jpg" alt="Stranger" width="30%" height="30%"/>
+    <h3>Stranger Things</h3>
+    <h3>Rating:8.5</h3>
+    <p>This is one of the best Series on Netfelix</p>
+  </div>)
+}
--- Stranger/src/App.jsx
+++ Stranger/src/App.jsx
@@ -14,13 +14,13 @@ export const App = ()=>{
 export const NetflixSeries =()=>{
   return (
     <div> 
-   <div>
-     <img src="Stranger.jpg" alt="stranger"  width="40%" height="20%"/>
-   </div>
-     <h2>Name: Strangers Things</h2>
-     <h3>Rating: 8.7</h3>
-     <p> Summary : This is a fantastic series. This is a fantasy series. </p>
-  </div>
+      <div>
+        <img src="Stranger.jpg" alt="stranger"  width="40%" height="20%"/>
+      </div>
+        <h2>Name: Strangers Things</h2>
+        <h3>Rating: 8.7</h3>
+        <p> Summary : This is a fantastic series. This is a fantasy series. </p>
+    </div>
   )
 
 }

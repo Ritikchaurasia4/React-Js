a293e22c7d47575e34489d8a4273cdabf2c6194b
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-07-17 23:02:42 +0530

    Some interview question in React

--- Pracctices-Interview/src/Practices.jsx
+++ Pracctices-Interview/src/Practices.jsx
@@ -1,3 +1,25 @@
 export const Practices =()=>{
-    return <h1> InterView Questions </h1>;
+    const Students = [];
+    console.log(Boolean(Students.length));
+    console.log(Students.length);
+    return(
+        <>       
+          {/* <p>{Students.length && "No Students Found"}</p> */}
+
+          {/* first solution........... */}
+          
+          {/* <p>{Students.length===0 && "No Students Found"}</p> */}
+
+          {/* second solution.......... */}
+
+          {/* <p>{!Students.length && "No Students Found"}</p> */}
+
+          {/* third solution.................. */}
+
+          <p>{!Boolean(Students.length) && "No Students Found"}</p>
+
+          <p> Number of The Students : {Students.length} </p>
+        </>
+
+    );
 };
---
+++ Pracctices-Interview/src/source.md
@@ -0,0 +1,4 @@
+React does not render 'fales' , 'undefined' , 'NaN' , 'null' in the DOM . These values , when used in jsx , will result in nothing being displayed.
+
+
+However , '0'  and empty strings (` " " `) are exceptions .

a1fe8b6bd420d7eba2c40891a3a51f81c9850bb0
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-07-22 19:56:14 +0530

    loop in React

--- Netfelix-MovieSeries/src/components/NetflixSeries.jsx
+++ Netfelix-MovieSeries/src/components/NetflixSeries.jsx
@@ -16,8 +16,58 @@ export const NetflixSeries =()=>{
   
           <p>Genre:{seriesData[0].genre}</p>
           <p> Caste: :{seriesData[0].cast} </p>
-          <a href={seriesData[0].watch_url} target="_blank"></a>
-          <button>watch Now</button>
+       <button>   <a href={seriesData[0].watch_url} target="_blank">Watch Now</a>  </button>
+       <div>
+          <img src={seriesData[1].img_url} alt="stranger"  width="40%" height="20%"/>
+        </div>
+           <h2>Name: {seriesData[1].name}</h2>
+          <h3>Rating: {seriesData[1].rating}</h3>
+          <p> Summary :{seriesData[1].description} </p>
+  
+          <p>Genre:{seriesData[1].genre}</p>
+          <p> Caste: :{seriesData[1].cast} </p>
+       <button>   <a href={seriesData[1].watch_url} target="_blank">Watch Now</a>  </button>
+       <div>
+          <img src={seriesData[2].img_url} alt="stranger"  width="40%" height="20%"/>
+        </div>
+           <h2>Name: {seriesData[2].name}</h2>
+          <h3>Rating: {seriesData[2].rating}</h3>
+          <p> Summary :{seriesData[2].description} </p>
+  
+          <p>Genre:{seriesData[2].genre}</p>
+          <p> Caste: :{seriesData[2].cast} </p>
+       <button>   <a href={seriesData[2].watch_url} target="_blank">Watch Now</a>  </button>
+       <div>
+          <img src={seriesData[3].img_url} alt="stranger"  width="40%" height="20%"/>
+        </div>
+           <h2>Name: {seriesData[3].name}</h2>
+          <h3>Rating: {seriesData[3].rating}</h3>
+          <p> Summary :{seriesData[3].description} </p>
+  
+          <p>Genre:{seriesData[3].genre}</p>
+          <p> Caste: :{seriesData[3].cast} </p>
+       <button>   <a href={seriesData[3].watch_url} target="_blank">Watch Now</a>  </button>
+       <div>
+          <img src={seriesData[4].img_url} alt="stranger"  width="40%" height="20%"/>
+        </div>
+           <h2>Name: {seriesData[4].name}</h2>
+          <h3>Rating: {seriesData[4].rating}</h3>
+          <p> Summary :{seriesData[4].description} </p>
+  
+          <p>Genre:{seriesData[4].genre}</p>
+          <p> Caste: :{seriesData[4].cast} </p>
+        <div>
+          <img src={seriesData[5].img_url} alt="stranger"  width="40%" height="20%"/>
+        </div>
+           <h2>Name: {seriesData[5].name}</h2>
+          <h3>Rating: {seriesData[5].rating}</h3>
+          <p> Summary :{seriesData[5].description} </p>
+  
+          <p>Genre:{seriesData[5].genre}</p>
+          <p> Caste: :{seriesData[5].cast} </p>
+       <button>   <a href={seriesData[5].watch_url} target="_blank">Watch Now</a>  </button>
+             
+          
       </div>
     );
   
--- Stranger/src/Components/NetFlix.jsx
+++ Stranger/src/Components/NetFlix.jsx
@@ -2,72 +2,30 @@
  export const NetflixSeries =()=>{
 
  return (
+  
   <ul>
-    <li>  
-      
-      <div> 
+    {
+      seriesData.map((curElem)=>{
+        return(
+          // Each child in a list should have a "key" prop.
+       <li key={curElem.id}>  
        
-       <div>
-         <img src={seriesData[0].img_url} alt="stranger"  width="40%" height="20%"/>
-       </div>
-          <h2>Name: {seriesData[0].name}</h2>
-         <h3>Rating: {seriesData[0].rating}</h3>
-         <p> Summary :{seriesData[0].description} </p>
- 
-         <p>Genre:{seriesData[0].genre}</p>
-         <p> Caste: :{seriesData[0].cast} </p>
-         <a href={seriesData[0].watch_url} target="_blank" > <button>watch Now</button></a>
-     </div>
-    </li>
-
-    <li>  
-      
-      <div> 
-       
-       <div>
-         <img src={seriesData[1].img_url} alt="stranger"  width="40%" height="20%"/>
-       </div>
-          <h2>Name: {seriesData[1].name}</h2>
-         <h3>Rating: {seriesData[1].rating}</h3>
-         <p> Summary :{seriesData[1].description} </p>
- 
-         <p>Genre:{seriesData[1].genre}</p>
-         <p> Caste: :{seriesData[1].cast} </p>
-         <a href={seriesData[1].watch_url} target="_blank" > <button>watch Now</button></a>
          
-     </div>
-    </li>
-    <div> 
+         <div>
+           <img src={curElem.img_url} alt="movie title name"  width="40%" height="20%"/>
+         </div>
+            <h2>Name: {curElem.name}</h2>
+           <h3>Rating: {curElem.rating}</h3>
+           <p> Summary :{curElem.description} </p>
+   
+           <p>Genre:{curElem.genre}</p>
+           <p> Caste: :{curElem.cast} </p>
+           <a href={curElem.watch_url} target="_blank" > <button>watch Now</button></a>
        
-       <div>
-         <img src={seriesData[2].img_url} alt="stranger"  width="40%" height="20%"/>
-       </div>
-          <h2>Name: {seriesData[2].name}</h2>
-         <h3>Rating: {seriesData[2].rating}</h3>
-         <p> Summary :{seriesData[2].description} </p>
- 
-         <p>Genre:{seriesData[2].genre}</p>
-         <p> Caste: :{seriesData[2].cast} </p>
-         <a href={seriesData[2].watch_url} target="_blank" > <button>watch Now</button></a>
-     </div>
-    <li>  
-      
-      <div> 
-       
-       <div>
-         <img src={seriesData[3].img_url} alt="stranger"  width="40%" height="20%"/>
-       </div>
-          <h2>Name: {seriesData[3].name}</h2>
-         <h3>Rating: {seriesData[3].rating}</h3>
-         <p> Summary :{seriesData[3].description} </p>
- 
-         <p>Genre:{seriesData[3].genre}</p>
-         <p> Caste: :{seriesData[3].cast} </p>
-         <a href={seriesData[3].watch_url} target="_blank" > <button>watch Now</button></a>
-         
-     </div>
-    </li>
-  </ul>
+      </li>
+      );
+      })}
+   </ul>
    );
  
 

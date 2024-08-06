d9c3cd0739f4ab251e52797b32371e58de1dad0c
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-07 00:29:53 +0530

    useState to update values

---
+++ UseState/src/App.jsx
@@ -0,0 +1,10 @@
+import "./components/EV.css";
+import {State} from "./components/hooks";
+export const App =()=>{
+  return (
+      <section>
+        <h1></h1>
+        <State/>
+      </section>
+  );
+};
---
+++ UseState/src/components/hooks.jsx
@@ -0,0 +1,49 @@
+// export const State = ()=>{
+//    let value = 0;
+//     const handleButton =()=>{
+//         value++;
+//         console.log(value);
+//     }
+//     return(
+//         <>
+//         <section className="main-div">
+//             <h1>{value}</h1>
+//             <button onClick={handleButton}>Increment</button>
+//         </section>
+//         </>
+//     );
+// };
+
+
+
+// In Above code -------------------
+// React does not know the normal variable , thats it going to be changed .
+// And that react should respond or update DOM based on that . Those values always rendered in DOM , There is no away they will be re-rendered . 
+// A normal variable does not work in ReactJs .
+
+// To resolve this we use useState in react
+
+
+// =========================================================================================================================
+
+import { useState } from "react";
+export const State = ()=>{
+
+//    let array = useState();
+//    console.log(array);
+
+   const [count , setCount] = useState(0);
+
+   const handleButtonClick =()=>{
+    setCount(() => count + 1);
+   }
+
+    return(
+         <>
+         <section className="main-div">
+             <h1>{count}</h1>
+             <button onClick={handleButtonClick}>Increment</button>
+         </section>
+         </>
+     );
+ };
